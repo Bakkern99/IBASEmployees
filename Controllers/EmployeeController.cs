f4b4db5f8dfa91cc1ff8f3092c7694aa2e9b75df
Jakob <j.bstephansen1@gmail.com>
2025-11-07 10:31:43 +0100

    Tilføjet 3 IT-ansatte og 2 kantineansatte

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -5,6 +5,8 @@ namespace IBASEmployeeService.Controllers
     
     [ApiController]
     [Route("api/[controller]")]
+    //controller bliver altså slettet fra EmployeeController, så jeg skal 
+    //skrive api/Employee/GetEmployees
     public class EmployeeController : ControllerBase
     {
         private readonly ILogger<EmployeeController> _logger;
@@ -45,6 +47,53 @@ namespace IBASEmployeeService.Controllers
                     Name = "Support"
                 }
             }
+            new Employee() {
+            Id = "24",
+            Name = "Peter Jensen",
+            Email = "peje@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "it"
+            }
+        },
+        new Employee() {
+            Id = "25",
+            Name = "Anna Nielsen",
+            Email = "anni@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "it"
+            }
+        },
+        new Employee() {
+            Id = "26",
+            Name = "Thomas Larsen",
+            Email = "thla@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "it"
+            }
+        },
+        
+        // NYE ansatte i Kantinen
+        new Employee() {
+            Id = "27",
+            Name = "Maria Andersen",
+            Email = "maan@ibas.dk",
+            Department = new Department() {
+                Id = 4,
+                Name = "kantinen"
+            }
+        },
+        new Employee() {
+            Id = "28",
+            Name = "Lars Pedersen",
+            Email = "lape@ibas.dk",
+            Department = new Department() {
+                Id = 4,
+                Name = "kantinen"
+            }
+        }
         };
             return employees;
         }
